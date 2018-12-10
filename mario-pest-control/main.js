861d222043ef2d251d445908dfd38ff473f0d316
trevorsteinle <trevorsteinle@gmail.com>
2018-12-10 13:16:50 -0700

    slight change in styling, still needs work

--- mario-pest-control/main.js
+++ mario-pest-control/main.js
@@ -14,6 +14,6 @@ document.total.addEventListener("submit", function(e) {
     document.total.appendChild(wrapper);  
     }
     var calc = goombacalc + bobombcalc + koopatroopacalc; 
-    wrapper.textContent = calc;
+    wrapper.textContent = calc + ' Coins';
     document.total.appendChild(wrapper);
 })

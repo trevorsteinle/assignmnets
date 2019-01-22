5113c9ddc9b10132ae5a0795042d95b528f9624a
Trevor Steinle <trevorsteinle@gmail.com>
2019-01-22 03:30:14 -0700

    countdown update

--- react-api-personal/src/MissionUpcomingDetail.js
+++ react-api-personal/src/MissionUpcomingDetail.js
@@ -19,14 +19,15 @@ function MissionUpcomingDetail({ details, flight_number, mission_name, launch_da
     // console.log(launch_site)
     // console.log(JSON.stringify(launch_site));
     // let currentUnix = Math.round((new Date()).getTime() / 1000)
-    let unixNow = Math.round((new Date()).getTime());
-    console.log(unixNow)
+    // let unixNow = Math.round((new Date()).getTime());
+    let launchUnix = (launch_date_unix * 1000);
+    // console.log(unixNow)
 
     return (
         <div style={styles}>
             <div>
                 {/* <img style={{display: 'block'}} src={links.mission_patch_small} alt={links.mission_patch_small}/> */}
-                <h1><Countdown date={unixNow + (launch_date_unix) }/></h1>
+                <h1><Countdown date={(launchUnix)}/></h1>
                 <h3>{mission_name}</h3>
                 {/* <p>{rocket.first_stage.cores[0].core_serial}</p> */}
                 <p >{details}</p>

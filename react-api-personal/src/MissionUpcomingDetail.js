a134a90b90e4473f4aaa4cbdd0d753d18ed659cf
Trevor Steinle <trevorsteinle@gmail.com>
2019-01-22 03:18:07 -0700

    add countdown

--- react-api-personal/src/MissionUpcomingDetail.js
+++ react-api-personal/src/MissionUpcomingDetail.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Countdown from 'react-countdown-now';
 
-function MissionUpcomingDetail({ details, flight_number, mission_name, launch_date_utc, links, rocket, launch_site }) {
+function MissionUpcomingDetail({ details, flight_number, mission_name, launch_date_utc, launch_date_unix, links, rocket, launch_site }) {
     // const styles={
     //     display: 'grid',
     //     gridTemplateColumns: 'repeat(3,1fr)'
@@ -16,13 +17,16 @@ function MissionUpcomingDetail({ details, flight_number, mission_name, launch_da
     }
 
     // console.log(launch_site)
-    console.log(JSON.stringify(launch_site));
-
+    // console.log(JSON.stringify(launch_site));
+    // let currentUnix = Math.round((new Date()).getTime() / 1000)
+    let unixNow = Math.round((new Date()).getTime());
+    console.log(unixNow)
 
     return (
         <div style={styles}>
             <div>
                 {/* <img style={{display: 'block'}} src={links.mission_patch_small} alt={links.mission_patch_small}/> */}
+                <h1><Countdown date={unixNow + (launch_date_unix) }/></h1>
                 <h3>{mission_name}</h3>
                 {/* <p>{rocket.first_stage.cores[0].core_serial}</p> */}
                 <p >{details}</p>
